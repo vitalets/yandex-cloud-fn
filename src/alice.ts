e24a912285cfc12b3fe37fc7af0baedefc2317e8
Vitaliy Potapov <noginsk@rambler.ru>
2021-10-28 23:19:41 +0300

    alice: keep alice req/res type name

--- src/alice.ts
+++ src/alice.ts
@@ -1,12 +1,14 @@
 /**
  * Интеграция с Алисой.
  */
-import { ReqBody as AliceRequest, ResBody as AliceResponse } from 'alice-types';
+import { ReqBody, ResBody } from 'alice-types';
 import { Handler } from '.';
 
 export function isAliceRequest(event: unknown): event is AliceRequest {
   return Boolean((event as AliceRequest).request);
 }
 
-export { AliceRequest, AliceResponse };
+// Use fake Omit to have AliceRequest/AliceResponse in TS messages.
+export type AliceRequest = Omit<ReqBody, ''>;
+export type AliceResponse = Omit<ResBody, ''>;
 export type AliceHandler = Handler<AliceRequest, AliceResponse>;
